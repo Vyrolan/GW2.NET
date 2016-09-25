1d97df37bf3e4c40aa1b5ec1583848e05196b19f
Robert Logiewa <Ruhrpottpatriot@gmx.de>
2016-09-25 21:42:06 +0200

    Move IRepository to namespace "Common"

--- src/GW2NET.Core/Common/IRepository.cs
+++ src/GW2NET.Core/Common/IRepository.cs
@@ -1,16 +1,16 @@
-﻿// <copyright file="IRepository.cs" company="GW2.NET Coding Team">
+// <copyright file="IRepository.cs" company="GW2.NET Coding Team">
 // This product is licensed under the GNU General Public License version 2 (GPLv2). See the License in the project root folder or the following page: http://www.gnu.org/licenses/gpl-2.0.html
 // </copyright>
 
-namespace GW2NET.Connectivity
+namespace GW2NET.Common
 {
     using System;
-    using Common;
+    using Connectivity;
 
     /// <summary>Describes a single api data source.</summary>
     /// <typeparam name="TDataContract">The type the api data is stored in.</typeparam>
     /// <typeparam name="TValue">The type the data is retrieved with.</typeparam>
-    public interface IRepository<TDataContract, out TValue>
+    public interface IRepository<in TDataContract, out TValue>
     {
         /// <summary>Gets the <see cref="GW2NET.Connectivity.Connector"/> used to make queries against the data source.
         /// </summary>
