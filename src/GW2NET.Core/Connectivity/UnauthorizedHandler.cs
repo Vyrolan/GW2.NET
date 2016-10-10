b2342eaeacefe9b75136afca0212f0e12659939d
Robert Logiewa <Ruhrpottpatriot@gmx.de>
2016-10-10 15:59:23 +0200

    Add bare bones unauthorized handler

---
+++ src/GW2NET.Core/Connectivity/UnauthorizedHandler.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="UnauthorizedHandler.cs" company="GW2.NET Coding Team">
+// This product is licensed under the GNU General Public License version 2 (GPLv2). See the License in the project root folder or the following page: http://www.gnu.org/licenses/gpl-2.0.html
+// </copyright>
+
+namespace GW2NET.Connectivity
+{
+    using System.Net.Http;
+
+    public class UnauthorizedHandler : DelegatingHandler
+    {
+        public UnauthorizedHandler(HttpClientHandler httpClientHandler)
+        {
+            this.InnerHandler = httpClientHandler;
+        }
+    }
+}
